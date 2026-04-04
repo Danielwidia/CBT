accf0bf45e72adf592a2b61ccbac338e23ae813a
Danielwidia <96729309+Danielwidia@users.noreply.github.com>
2026-04-04 21:53:16 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -196,6 +196,7 @@ async function insertResultSingle(resultObj) {
 // ─── Static Files ─────────────────────────────────────────────────────────────
 app.get('/',         (req, res) => res.sendFile(path.join(rootPath, 'index.html')));
 app.get('/logo.png', (req, res) => res.sendFile(path.join(rootPath, 'logo.png')));
+app.get('/administrasi_guru.html', (req, res) => res.sendFile(path.join(rootPath, 'administrasi_guru.html')));
 
 app.use((req, res, next) => { console.log(`${req.method} ${req.url}`); next(); });
 
